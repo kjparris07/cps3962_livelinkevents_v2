4ba8897dee3eaa05d60c35a8472794f3c400a6b7
kjparris07 <kjparris07@gmail.com>
2026-03-26 14:59:31 -0400

    converted member page to tsx

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -1,11 +1,30 @@
 import "../../styles/main.css";
 import "../../styles/membership.css";
+import { BigButton } from "../globalComponents/BigButton";
 
 export default function Home() {
   return (
     <main>
-      <div className="container">
-        <h1>Join LiveLink</h1>
+      <div className="page">
+        <div className="hero-text">
+          <h1>UPGRADE YOUR EXPERIENCE AND<br/>BECOME A MEMBER TODAY!</h1>
+        </div>
+        <div className="white-background"> 
+          <div className="subtitle">Members enjoy:</div>  
+          <ul className="benefits">
+            <li>Higher-quality seats that offer closeness to the stage, a better view, and extra comfort.</li>
+            <li>Early notifications.</li>
+            <li>Enhanced alerts, including price drop.</li>
+            <li>Exclusive deals on tickets and artist merchandise.</li>
+            <li>Priority access to tickets for high-demand events. </li>
+            <li>Exclusive access to members-only events, such as pre-sales, meet-and-greets, and special performances.</li>
+            <li>Early access to tickets before general sale starts.</li>
+          </ul>
+        </div>
+        <BigButton
+          content={"Become a member!"}
+          link={"/payment"}
+        />
       </div>
     </main>
   );
---
+++ app/payment/page.tsx
@@ -0,0 +1,12 @@
+import "../../styles/main.css";
+import "../../styles/payment.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="container">
+        <h1>PAYMENT</h1>
+      </div>
+    </main>
+  );
+}
